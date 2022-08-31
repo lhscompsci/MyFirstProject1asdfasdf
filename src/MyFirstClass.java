6f88749f4d90b49e78c658ad272599b86db0241f
Sam Black <sam.black@lubbockisd.org>
2022-08-31 09:46:23 -0500

    Initial commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,6 @@
+public class MyFirstClass {
+    public static void main(String[] args) {
+        System.out.print("Good morning, coders!");
+        System.out.println("It's a beautiful rainy Tuesday!");
+    }
+}
