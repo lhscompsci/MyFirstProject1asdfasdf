e8a62c70137e9df22946b405d3b532699033ff93
Sam Black <sam.black@lubbockisd.org>
2022-09-01 08:28:17 -0500

    message updated to reflect new day

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -1,6 +1,7 @@
 public class MyFirstClass {
     public static void main(String[] args) {
         System.out.print("Good morning, coders!");
-        System.out.println("It's a beautiful rainy Tuesday!");
+        System.out.println("It's a beautiful cloudy Thursday!");
+
     }
 }
